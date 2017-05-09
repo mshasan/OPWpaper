ad9e46535dc0375338a2fa0ed9d7a933107d2238
Mohamad Shakil Hasan <shakilmohamad7@gmail.com>
2017-05-09 16:57:41 -0400

    fwer updated

--- inst/simulation_benchmarks/simu_fwer.R
+++ inst/simulation_benchmarks/simu_fwer.R
@@ -29,3 +29,29 @@ stopCluster(cl)
 # save the workspace to the file .RData in the cwd
 save.image("smu_fwer.RData")
 
+
+
+
+
+fwer_by_alpha <- matrix(apply(fwer_mat, 1, mean), nrow = 4, byrow = FALSE)
+
+
+alphaVal = seq(.01, .1, .02)
+datError <- data.frame(alphaVal, t(fwer_by_alpha))
+colnames(datError) <- c("alpha","BON","PRO_bin","PRO_cont", "IHW")
+datError2 <- melt(datError, id.var="alpha")
+
+panel_d = ggplot(datError2, aes(x = alpha, y = value, col=variable)) +
+    geom_line(size=1.5) +
+    geom_abline(linetype="dashed") +
+    xlab(expression(bold(paste("Nominal ",alpha)))) +
+    ylab("FWER")+
+    scale_x_continuous(limits = c(0.01,0.1), breaks=seq(0.01,0.09,length=5)) +
+    #ylim(0,0.9) +
+    theme(legend.title = element_blank())+
+    theme(axis.title = element_text(face="bold"))+
+    theme(panel.background = element_rect(fill = 'white', colour = 'black'))
+
+#df = data.frame(colour=mycolours, last_vals=c(0.08,   0.085,   0.09), label=c("BON","PRO_bin","PRO_cont"))
+#panel_d <- pretty_legend(panel_d, df, .092)
+panel_d
